580a647b53ff3056acd31c67ef7602a2f9df56b7
Bekzat <abdilamituulubekzat07@gmail.com>
2026-03-07 21:26:06 +0600

    Dokcerize

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,9 +17,13 @@ from .routers import (
 
 app = FastAPI(title="Flashcards API")
 
+origins = [
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # dev only
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
